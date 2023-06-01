dbdb9196bf05091277302cff734837e8384f5f56
ignaarguello <igna.arguello2110@hotmail.com>
2023-06-01 20:33:44 -0300

    animaciones a los titulosdel home

--- app/components/Home_P1/Home_P1.jsx
+++ app/components/Home_P1/Home_P1.jsx
@@ -1,4 +1,4 @@
-
+import 'animate.css';
 
 export default function Home_P1() {
   const var_text = `< Desarrollador Web />`
@@ -7,8 +7,8 @@ export default function Home_P1() {
     <div id="container-general__Home_P1">
       <img id="image-home_P1" src="../images/Argüello Ignacio.jpg" alt="" />
       <div id="container-info__Home-P1">
-        <h2 className="item-text__Home-P1">Argüello Ignacio</h2>
-        <h3 className="item-text__Home-P1">{var_text}</h3>
+        <h2 className="item-text__Home-P1 animate__animated animate__slideInRight">Argüello Ignacio</h2>
+        <h3 className="item-text__Home-P1 animate__animated animate__bounceIn animate__delay-1s animate__slow">{var_text}</h3>
         {/* <div id="button-descargarCV__Home__P1">Descargar CV</div> */}
       </div>
     </div>
