4c9969047da19c1feec05827b90507b3f7ac8430
ignaarguello <igna.arguello2110@hotmail.com>
2023-06-02 15:09:27 -0300

    avances en home - perfect

---
+++ app/components/Home_P2/Home_P2.jsx
@@ -0,0 +1,7 @@
+import '../../styles/styles.css'
+
+export default function Home_P2() {
+  return (
+    <div id="container-general__Home__P2">Home_P2</div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Home_P1 from './components/Home_P1/Home_P1'
+import Home_P2 from './components/Home_P2/Home_P2'
 
 export default function Home() {
   return (
     <>
       <Home_P1 />
+      <Home_P2 />
     </>
   )
 }
