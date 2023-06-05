aab02a5b4c58cc1ba74c4f6fc2da002ad0fa3acc
ignaarguello <igna.arguello2110@hotmail.com>
2023-06-05 15:55:46 -0300

    correcion en medida vh en home-1

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Home_P2 from './components/Home_P2/Home_P2'
 
 export default function Home() {
   return (
-    <>
+    <div className='flex flex-col'>
       <Home_P1 />
       <Home_P2 />
-    </>
+    </div>
   )
 }
