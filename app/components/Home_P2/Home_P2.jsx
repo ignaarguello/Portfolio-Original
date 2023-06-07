dfc389dd95d67a01d56006c20562b7d82f62829c
ignaarguello <igna.arguello2110@hotmail.com>
2023-06-07 18:27:45 -0300

    more corrections in 360px

--- app/components/Home_P2/Home_P2.jsx
+++ app/components/Home_P2/Home_P2.jsx
@@ -4,7 +4,7 @@ import { SiCss3 } from "react-icons/si";
 import { FaNodeJs } from "react-icons/fa";
 import { SiExpress } from "react-icons/si";
 import { DiJavascript1 } from "react-icons/di";
-import { DiReact} from "react-icons/di";
+import { DiReact } from "react-icons/di";
 import { SiMongodb } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 import { AiOutlineApi } from "react-icons/ai";
@@ -16,8 +16,8 @@ export default function Home_P2() {
     { "nombre": 'html', "logo": <FaHtml5 className='logo-skill' />, },
     { "nombre": 'css', "logo": <SiCss3 className='logo-skill' />, },
     { "nombre": 'javascript', "logo": <DiJavascript1 className='logo-skill' />, },
-    { "nombre": 'react', "logo": <DiReact  className='logo-skill' />, },
-    { "nombre": 'NextJS', "logo": <TbBrandNextjs  className='logo-skill' />, },
+    { "nombre": 'react', "logo": <DiReact className='logo-skill' />, },
+    { "nombre": 'NextJS', "logo": <TbBrandNextjs className='logo-skill' />, },
   ]
 
   const skillsBackend = [
@@ -28,7 +28,7 @@ export default function Home_P2() {
     { "nombre": 'api rest', "logo": <AiOutlineApi className='logo-skill' />, },
   ]
 
-  const alternativeSkills = skillsFrontend.slice(4,6)
+  const alternativeSkills = skillsFrontend.slice(4, 6)
 
   return (
     <div id="container-general__Home__P2">
@@ -37,7 +37,7 @@ export default function Home_P2() {
         {/* Comienzo de contenedor FRONTEND */}
         {
           skillsFrontend.map(elem =>
-            <div className='my-3'>
+            <div className='mt-2'>
               <h2 className='titulo-stack__Home-P2'>{elem?.titulo}</h2>
               <div className='cont-eachSkill__Home-P2'>
                 <h2 className='nombre-skill'>{elem?.nombre?.toUpperCase()}</h2>
@@ -49,13 +49,13 @@ export default function Home_P2() {
         {/* Comienzo de contenedor BACKEND */}
         {
           skillsBackend.map(elem =>
-            <>
+            <div className='mt-4'>
               <h2 className='titulo-stack__Home-P2'>{elem?.titulo}</h2>
               <div className='cont-eachSkill__Home-P2'>
                 <h2 className='nombre-skill'>{elem?.nombre?.toUpperCase()}</h2>
                 {elem?.logo}
               </div>
-            </>
+            </div>
           )
         }
       </div>
