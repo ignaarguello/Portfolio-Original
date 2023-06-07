cef29951f652098fd1ab5d4cd59566b13eb90ff4
ignaarguello <igna.arguello2110@hotmail.com>
2023-06-07 18:06:35 -0300

    mas logos en skills

--- app/components/Home_P2/Home_P2.jsx
+++ app/components/Home_P2/Home_P2.jsx
@@ -4,7 +4,10 @@ import { SiCss3 } from "react-icons/si";
 import { FaNodeJs } from "react-icons/fa";
 import { SiExpress } from "react-icons/si";
 import { DiJavascript1 } from "react-icons/di";
+import { DiReact} from "react-icons/di";
 import { SiMongodb } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+import { AiOutlineApi } from "react-icons/ai";
 
 export default function Home_P2() {
 
@@ -13,14 +16,19 @@ export default function Home_P2() {
     { "nombre": 'html', "logo": <FaHtml5 className='logo-skill' />, },
     { "nombre": 'css', "logo": <SiCss3 className='logo-skill' />, },
     { "nombre": 'javascript', "logo": <DiJavascript1 className='logo-skill' />, },
+    { "nombre": 'react', "logo": <DiReact  className='logo-skill' />, },
+    { "nombre": 'NextJS', "logo": <TbBrandNextjs  className='logo-skill' />, },
   ]
 
   const skillsBackend = [
     { titulo: 'Backend' },
-    { "nombre": 'Node', "logo": <FaNodeJs className='logo-skill' />, },
-    { "nombre": 'Express', "logo": <SiExpress className='logo-skill' />, },
-    { "nombre": 'MongoDB', "logo": <SiMongodb className='logo-skill' />, },
+    { "nombre": 'node', "logo": <FaNodeJs className='logo-skill' />, },
+    { "nombre": 'express', "logo": <SiExpress className='logo-skill' />, },
+    { "nombre": 'mongo DB', "logo": <SiMongodb className='logo-skill' />, },
+    { "nombre": 'api rest', "logo": <AiOutlineApi className='logo-skill' />, },
   ]
+
+  const alternativeSkills = skillsFrontend.slice(4,6)
 
   return (
     <div id="container-general__Home__P2">
