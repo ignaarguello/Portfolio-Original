7805a7646ade79ca1c06767ff26ea83f45429ee5
ignaarguello <igna.arguello2110@hotmail.com>
2023-06-16 17:20:34 -0300

    social media listos

--- app/components/Home_P4/Home_P4.jsx
+++ app/components/Home_P4/Home_P4.jsx
@@ -1,9 +1,39 @@
 import '../../styles/styles.css'
+import { FiInstagram } from "react-icons/fi";
+import { GrLinkedinOption } from "react-icons/gr";
+import { BsGithub } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
 
 export default function Home_P4() {
     return (
         <div id="container-general__Home-P4">
-
+            <h2 id='titulo-contacto__Home-P4'>Contacto</h2>
+            <div class="contenedor-grid-socialMedia__Home-P4">
+                <div class="item-grid-socialMedia__Home-P4">
+                    <div className='cont-logo-socialMed__Home-P4'>
+                        <FiInstagram className='logo-socialMedia__Home-P4' />
+                    </div>
+                    <h3>Instagram</h3>
+                </div>
+                <div class="item-grid-socialMedia__Home-P4">
+                    <div className='cont-logo-socialMed__Home-P4'>
+                        <GrLinkedinOption className='logo-socialMedia__Home-P4' />
+                    </div>
+                    <h3>Linkedin</h3>
+                </div>
+                <div class="item-grid-socialMedia__Home-P4">
+                    <div className='cont-logo-socialMed__Home-P4'>
+                        <BsGithub className='logo-socialMedia__Home-P4' />
+                    </div>
+                    <h3>Github</h3>
+                </div>
+                <div class="item-grid-socialMedia__Home-P4">
+                    <div className='cont-logo-socialMed__Home-P4'>
+                        <BsWhatsapp className='logo-socialMedia__Home-P4' />
+                    </div>
+                    <h3>Whatsapp</h3>
+                </div>
+            </div>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Home_P1 from './components/Home_P1/Home_P1'
 import Home_P2 from './components/Home_P2/Home_P2'
 import Home_P3 from './components/Home_P3/Home_P3'
+import Home_P4 from './components/Home_P4/Home_P4'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Home_P1 />
       <Home_P2 />
       <Home_P3 />
+      <Home_P4 />
     </div>
   )
 }
