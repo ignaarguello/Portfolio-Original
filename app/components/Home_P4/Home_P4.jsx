d205e82785faa91fbeed760d26027cce0b9e7432
ignaarguello <igna.arguello2110@hotmail.com>
2023-06-16 21:23:09 -0300

    correcion en los refs de home p4 - eliminacion de target blank

--- app/components/Home_P4/Home_P4.jsx
+++ app/components/Home_P4/Home_P4.jsx
@@ -3,36 +3,37 @@ import { FiInstagram } from "react-icons/fi";
 import { GrLinkedinOption } from "react-icons/gr";
 import { BsGithub } from "react-icons/bs";
 import { BsWhatsapp } from "react-icons/bs";
+import Link from 'next/link';
 
 export default function Home_P4() {
     return (
         <div id="container-general__Home-P4">
             <h2 id='titulo-contacto__Home-P4'>Contacto</h2>
             <div class="contenedor-grid-socialMedia__Home-P4">
-                <div class="item-grid-socialMedia__Home-P4">
+                <Link href='https://www.instagram.com/ignaarguello_/' class="item-grid-socialMedia__Home-P4">
                     <div className='cont-logo-socialMed__Home-P4'>
                         <FiInstagram className='logo-socialMedia__Home-P4' />
                     </div>
                     <h3>Instagram</h3>
-                </div>
-                <div class="item-grid-socialMedia__Home-P4">
+                </Link>
+                <Link target='_blank' href='https://www.linkedin.com/in/arguelloignacio/' class="item-grid-socialMedia__Home-P4">
                     <div className='cont-logo-socialMed__Home-P4'>
                         <GrLinkedinOption className='logo-socialMedia__Home-P4' />
                     </div>
                     <h3>Linkedin</h3>
-                </div>
-                <div class="item-grid-socialMedia__Home-P4">
+                </Link>
+                <Link target='_blank' href='https://github.com/ignaarguello' class="item-grid-socialMedia__Home-P4">
                     <div className='cont-logo-socialMed__Home-P4'>
                         <BsGithub className='logo-socialMedia__Home-P4' />
                     </div>
                     <h3>Github</h3>
-                </div>
-                <div class="item-grid-socialMedia__Home-P4">
+                </Link>
+                <Link href='https://api.whatsapp.com/send?phone=1128682405' class="item-grid-socialMedia__Home-P4">
                     <div className='cont-logo-socialMed__Home-P4'>
                         <BsWhatsapp className='logo-socialMedia__Home-P4' />
                     </div>
                     <h3>Whatsapp</h3>
-                </div>
+                </Link>
             </div>
         </div>
     )
