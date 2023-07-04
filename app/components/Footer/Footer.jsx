63a6e255dcbb537f841e72f6f01bee7a5288f995
ignaarguello <igna.arguello2110@hotmail.com>
2023-07-04 17:31:58 -0300

    Footer y correciones en HomeP4

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,7 +1,17 @@
+import { MdCopyright } from "react-icons/md";
+
 export default function Footer() {
     return (
         <div id="container-general__Footer">
-            Footer
+            <div id="container-nameAndLogo__Footer">
+                <h2 className="text-footer__footer">Arguello Ignacio</h2>
+                <MdCopyright id="logo-copyright__footer" />
+            </div>
+            |
+            <div id="container-2__footer">
+                <h2 className="text-footer__footer">Quilmes - Argentina</h2>
+            </div>
+            <h2 className="text-footer__footer">Desarrollado con React - Next 13</h2>
         </div>
     )
 }
