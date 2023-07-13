340bd3f8cf5bca8372c3bf6776a899c7c4fc6925
ignaarguello <igna.arguello2110@hotmail.com>
2023-07-13 02:01:23 -0300

    Componente de HomeP1 - READY

--- app/components/Home_P1/Home_P1_Desktop.jsx
+++ app/components/Home_P1/Home_P1_Desktop.jsx
@@ -5,7 +5,6 @@ import ArabicSpanishText from '../ArabicSpanishText/ArabicSpanishText'
 
 
 export default function Home_P1_Desktop() {
-
   const TranslateElement = () => {
     const image_translate = document.querySelector('#Image-Main__HomeP1-DESKTOP')
     image_translate?.classList?.add('TranslateElement')
@@ -16,15 +15,11 @@ export default function Home_P1_Desktop() {
     image_translate?.classList?.remove('TranslateElement')
   }
 
-
   return (
     <div id="container-general__HomeP1-DESKTOP">
       <div className="contenedor-interior__HomeP1-DESKTOP">
         <div id='container-image-HomeP1-DESKTOP' onMouseEnter={TranslateElement} onMouseLeave={TranslateElementToOriginal}>
           <Image src="/images/foto-igna.jpg" alt="X" id='Image-Main__HomeP1-DESKTOP' width={50} height={50} sizes='100%' />
-          {/* <p id='text__HomeP1-DESKTOP'>
-            ¡Hola! Mi nombre es Ignacio Argüello, soy Desarrollador Web especializado en crear experiencias digitales cautivadoras. Con una sólida base en diseño y desarrollo, mi objetivo es fusionar la estética y la funcionalidad para ofrecer soluciones web únicas y atractivas. Siempre busco crear experiencias digitales personalizadas y centradas en el usuario. Soy apasionado por la creación de sitios web intuitivos, accesibles y visualmente impactantes. ¡Bienvenido a mi portfolio!
-          </p> */}
           <ArabicSpanishText />
         </div>
       </div>
