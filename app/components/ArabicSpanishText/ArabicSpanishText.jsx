4f01e50099f3e548ccad5e4a20da72f427243bd1
ignaarguello <igna.arguello2110@hotmail.com>
2023-07-14 18:08:12 -0300

    Add more styles in resolution - 1280(Desktop)

--- app/components/ArabicSpanishText/ArabicSpanishText.jsx
+++ app/components/ArabicSpanishText/ArabicSpanishText.jsx
@@ -20,7 +20,7 @@ const LanguageChange = () => {
             onMouseLeave={handleMouseLeave}
         >
             {isSpanish ? (
-                <span className={`spanish-text ${isSpanish ? 'fade-in' : ''}`}>¡Hola! Mi nombre es Ignacio Argüello, soy Desarrollador Web especializado en crear experiencias digitales cautivadoras. Con una sólida base en diseño y desarrollo, mi objetivo es fusionar la estética y la funcionalidad para ofrecer soluciones web únicas y atractivas. Siempre busco crear experiencias digitales personalizadas y centradas en el usuario. Soy apasionado por la creación de sitios web intuitivos, accesibles y visualmente impactantes. ¡Bienvenido a mi portfolio!</span>
+                <span className={`spanish-text ${isSpanish ? 'fade-in animate__slow' : ''}`}>¡Hola! Mi nombre es Ignacio Argüello, soy Desarrollador Web especializado en crear experiencias digitales cautivadoras. Con una sólida base en diseño y desarrollo, mi objetivo es fusionar la estética y la funcionalidad para ofrecer soluciones web únicas y atractivas. Siempre busco crear experiencias digitales personalizadas y centradas en el usuario. Soy apasionado por la creación de sitios web intuitivos, accesibles y visualmente impactantes. ¡Bienvenido a mi portfolio!</span>
             ) : (
                 <span className={`arabic-text ${!isSpanish ? 'animate__animated animate__fadeIn animate__slow' : ''}`}>مرحبا! اسمي جناسيو ، أنا مطور ويب متخصص في إنشاء تجارب رقمية آسرة. مع خلفية قوية في التصميم والتطوير ، هدفي هو دمج الجماليات والوظائف لتقديم حلول ويب فريدة وجذابة. أسعى دائمًا إلى إنشاء تجارب رقمية مخصصة تركز على المستخدم. أنا متحمس لإنشاء مواقع ويب سهلة الاستخدام ويمكن الوصول إليها ومذهلة بصريًا. مرحبا بكم في محفظتي!</span>
             )}
--- app/components/Home_P1/Home_P1_Desktop.jsx
+++ app/components/Home_P1/Home_P1_Desktop.jsx
@@ -1,7 +1,9 @@
 'use client'
+import { useEffect } from 'react'
 import '../../styles/styles.css'
 import Image from 'next/image'
 import ArabicSpanishText from '../ArabicSpanishText/ArabicSpanishText'
+import 'animate.css';
 
 
 export default function Home_P1_Desktop() {
@@ -15,11 +17,28 @@ export default function Home_P1_Desktop() {
     image_translate?.classList?.remove('TranslateElement')
   }
 
+  const TranslateAndShowElement = () => {
+    TranslateElement()
+    const second_image = document.querySelector('#Image-Main-Second__HomeP1-DESKTOP')
+    second_image?.classList?.add('TranslateAndShowElement-2')
+  }
+
+  const DeleteTranslateAndShowElement = () => {
+    TranslateElementToOriginal()
+    const second_image = document.querySelector('#Image-Main-Second__HomeP1-DESKTOP')
+    second_image?.classList?.remove('TranslateAndShowElement-2')
+  }
+
+  useEffect(() => {
+    TranslateAndShowElement()
+  }, [])
+
   return (
     <div id="container-general__HomeP1-DESKTOP">
       <div className="contenedor-interior__HomeP1-DESKTOP">
-        <div id='container-image-HomeP1-DESKTOP' onMouseEnter={TranslateElement} onMouseLeave={TranslateElementToOriginal}>
+        <div id='container-image-HomeP1-DESKTOP' className='animate__animated animate__fadeIn animate__delay-1s' /* onMouseEnter={TranslateAndShowElement} onMouseLeave={DeleteTranslateAndShowElement} */>
           <Image src="/images/foto-igna.jpg" alt="X" id='Image-Main__HomeP1-DESKTOP' width={50} height={50} sizes='100%' />
+          <Image src="/images/foto-igna3.png" alt="X" id='Image-Main-Second__HomeP1-DESKTOP' width={50} height={50} sizes='100%' />
           <ArabicSpanishText />
         </div>
       </div>
