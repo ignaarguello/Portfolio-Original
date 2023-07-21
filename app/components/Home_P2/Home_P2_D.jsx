d8b2bf03a2f05df940393490825199cb80040744
ignaarguello <igna.arguello2110@hotmail.com>
2023-07-21 14:22:35 -0300

    Correcion en z index de home P2 (DESKTOP)

--- app/components/Home_P2/Home_P2_D.jsx
+++ app/components/Home_P2/Home_P2_D.jsx
@@ -3,14 +3,16 @@ import { skillsFrontend, skillsBackend } from '../../data/data_skills'
 export default function Home_P2_Desktop() {
     return (
         <div id="container-general__HomeP2-D">
-            <div className="div-interiores__HomeP2-D">
-                <h2 className="title-skill__HomeP2-D">Frontend</h2>
-                <h2 className="title-skill__HomeP2-D">Backend</h2>
-            </div>
-            <div className="div-interiores__HomeP2-D">
-                <h2 className='title-eachSkill__HomeP2-D'>Habilidades</h2>
-                <div id='container-eachSkill__HomeP2-D'>
+            <div id='subContenedorGeneral_HomeP2-D'>
+                <div className="div-interiores__HomeP2-D">
+                    <h2 className="title-skill__HomeP2-D">Frontend</h2>
+                    <h2 className="title-skill__HomeP2-D">Backend</h2>
+                </div>
+                <div className="div-interiores__HomeP2-D">
+                    <h2 className='title-eachSkill__HomeP2-D'>Habilidades</h2>
+                    <div id='container-eachSkill__HomeP2-D'>
 
+                    </div>
                 </div>
             </div>
         </div>
