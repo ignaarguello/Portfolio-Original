359c9ec240db451b659b8ab0b393dc155e368d60
ignaarguello <igna.arguello2110@hotmail.com>
2023-08-03 16:39:20 -0300

    Todo listo para trabajar en HomeP3 - Desktop

---
+++ app/components/Home_P3/Home_P3_desktop.jsx
@@ -0,0 +1,9 @@
+import '../../styles/styles.css'
+
+export default function Home_P3_desktop() {
+    return (
+        <div id="contenedor-general__HomeP3-D">
+            Home P3 Desktopp
+        </div>
+    )
+}
---
+++ app/components/Home_P3/Home_P3_mobile.jsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import { BsGithub } from "react-icons/bs";
+import { BsFillShareFill } from "react-icons/bs";
+import { proyectos } from '../../data/data_projects'
+import { BsArrowRight } from "react-icons/bs";
+
+export default function Home_P3_mobile() {
+    return (
+        <div id="container-general__Home-P3">
+            <h2 id="titulo-proyectos__Home-P3">Proyectos Relevantes</h2>
+            <div id="contenedor-cards-projects__Home-P3">
+                {
+                    proyectos.map(elem =>
+                        <div key={elem.id} className="card-project__Home-P3" style={{
+                            backgroundImage: `url(${elem.imagen})`,
+                            backgroundSize: "cover",
+                            backgroundPosition: "center",
+                            backgroundRepeat: "no-repeat"
+                        }} >
+                            <h2 className='nombre_card_project__Home-P3'>{elem.nombre}</h2>
+                            <div className='container-refs__Home-P3'>
+                                <Link className='cont-ref__Home-P3' href={elem.url_gh} target="_blank">
+                                    <BsGithub className='btn-ref__Home-P3' />
+                                </Link>
+                                <Link className='cont-ref__Home-P3' href={elem.url_deploy} target="_blank">
+                                    <BsFillShareFill className='btn-ref__Home-P3' />
+                                </Link>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+            <Link id='container-ver-mas-proyectos__Home-P3' href='/pages/projects'>
+                <h2 id='titulo-ver-mas__Home-P3'>+ Ver más proyectos</h2>
+                <BsArrowRight id='flecha-right__Home-P3' />
+            </Link>
+        </div >
+    )
+}
