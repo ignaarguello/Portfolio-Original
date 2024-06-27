375e07c549603fd158f268d98883318dc8f31533
ignaarguello <igna.arguello2110@hotmail.com>
2024-06-27 14:08:12 -0300

    Add: ultimo proceso del proyecto

--- app/components/Home_P3/Home_P3_desktop.jsx
+++ app/components/Home_P3/Home_P3_desktop.jsx
@@ -4,8 +4,12 @@ import "../../styles/styles.css";
 import IndexPage from "../IndexPage/IndexPage";
 
 //Data
-//Data
 import { proyectos } from "../../data/data_projects";
+
+//Iconos y Librerias
+import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
+import { BsFillShareFill } from "react-icons/bs";
 
 export default function Home_P3_desktop() {
   return (
@@ -13,17 +17,36 @@ export default function Home_P3_desktop() {
       <IndexPage title="02.Proyectos" align="right" />
       <div id="contenedorProyectos_HomeP3_DESK">
         {proyectos.map((elem) => (
-          <div
-            className="CardProyecto_HomeP3"
-            key={elem.id}
-            style={{
-              backgroundImage: `url(${elem.imagen})`,
-              backgroundSize: "cover 50%",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-          >
-              <h2>{elem.nombre}</h2>
+          <div className="CardProyecto_HomeP3">
+            <div
+              className="ImageCard_HomeP3"
+              key={elem.id}
+              style={{
+                backgroundImage: `url(${elem.imagen})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            ></div>
+            <h2 className="TituloNombreCard_HomeP3">{elem.nombre}</h2>
+            <span>{elem.type}</span>
+            <div className="ContenedorIconosNavegacion_HomeP3">
+              <Link
+                className="cont-ref__Home-P3"
+                href={elem.url_gh}
+                target="_blank"
+              >
+                <BsGithub className="btn-ref__Home-P3" />
+              </Link>
+              <Link
+                className="cont-ref__Home-P3"
+                href={elem.url_deploy}
+                target="_blank"
+              >
+                <BsFillShareFill className="btn-ref__Home-P3" />
+              </Link>
+            </div>
+            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-4">Conocer más +</button>
           </div>
         ))}
       </div>
--- app/data/data_projects.js
+++ app/data/data_projects.js
@@ -1,27 +1,32 @@
-const proyectos = [
-    {
-        id:1,
+const proyectos = [{
+        id: 1,
         nombre: 'Salute Drinks',
         imagen: '/images/salute-drinks.png',
         url_gh: 'https://github.com/ignaarguello/Salute---front',
         url_deploy: 'https://salute-front.vercel.app/',
+        type: 'eCommerce',
     },
     {
-        id:2,
+        id: 2,
         nombre: 'Retro Team Football Clothing',
         imagen: '/images/rtfc.png',
         url_gh: 'https://github.com/nehuen-aumedes-diez/Mindhub-Challengue-RFTC-Front',
         url_deploy: 'https://rftc-web.vercel.app/',
+        type: 'eCommerce',
+
     },
     {
-        id:3,
+        id: 3,
         nombre: 'Eugenia - Nextia',
         imagen: '/images/eugenia2.png',
         url_gh: 'https://github.com/ignaarguello/Challenge-Nextia-MX---Front',
         url_deploy: 'https://nextiafront.vercel.app/sign-in',
+        type: 'Sistema de Gestión',
     },
 ]
 
 
 
-export { proyectos }
+export {
+    proyectos
+}
