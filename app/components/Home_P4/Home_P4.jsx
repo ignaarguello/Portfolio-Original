c12b0c20b2a4790cdee502fac9152abc392a8263
ignaarguello <igna.arguello2110@hotmail.com>
2024-07-17 15:44:36 -0300

    Add: Primeras modificaciones en componente HomeP4 (Contacto) - Desktop

--- app/components/Home_P4/Home_P4.jsx
+++ app/components/Home_P4/Home_P4.jsx
@@ -1,21 +1,31 @@
+/* Estilos */
 import '../../styles/styles.css'
+
+/* Iconos */
 import { BsPatchCheckFill } from "react-icons/bs";
+
+/* Componentes */
 import SocialMediaGrid from '../SocialMediaGrid/SocialMediaGrid'
 import FormContact from '../FormContact/FormContact'
+import IndexPage from '../IndexPage/IndexPage';
+
 
 export default function Home_P4() {
     return (
         <div id="container-general__Home-P4">
-            <div id='container-titleAndLogo__Home-P4'>
-                <h2 id='titulo-contacto__Home-P4'>Contacto</h2>
-                <BsPatchCheckFill id='logo-contacto__Home-P4' />
-            </div>
-            <FormContact />
-            <hr className="hr" />
-            <hr className="hr" />
-            <hr className="hr" />
-            <h4 id='texto-contacto__Home-P4'>Te invito a conectarte conmigo a través de mis redes sociales.</h4>
-            <SocialMediaGrid />
+            {/* Seccion - 1 */}
+            <section id='seccion-1__HomeP4'>
+                <div id='container-titleAndLogo__Home-P4'>
+                    <IndexPage title='04.Contacto' color='black' align='right' />
+                </div>
+                {/* <h3 id='TituloEmail__HomeP4'>・El email será respondido a la brevedad</h3> */}
+                <FormContact />
+            </section>
+            {/* Seccion - 2 */}
+            <section id='seccion-2__HomeP4'>
+                <h4 id='texto-contacto__Home-P4'>Te invito a conectarte conmigo a través de mis redes sociales.</h4>
+                <SocialMediaGrid />
+            </section>
         </div>
     )
 }
