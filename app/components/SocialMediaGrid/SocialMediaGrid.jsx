9064ef91b5f9536a8634293d4b9252c195870448
ignaarguello <igna.arguello2110@hotmail.com>
2024-07-23 00:30:03 -0300

    feat: modificaciones en el orden de componentes de home, mejoras en formulario y mejoras de estilos

--- app/components/SocialMediaGrid/SocialMediaGrid.jsx
+++ app/components/SocialMediaGrid/SocialMediaGrid.jsx
@@ -8,23 +8,17 @@ import Link from 'next/link';
 export default function SocialMediaGrid() {
     return (
         <div className="contenedor-grid-socialMedia__Home-P4">
-            <Link href='https://www.instagram.com/ignaarguello_/' class="item-grid-socialMedia__Home-P4">
+            <Link target='_blank' href='https://github.com/ignaarguello' class="item-grid-socialMedia__Home-P4">
                 <div className='cont-logo-socialMed__Home-P4'>
-                    <FiInstagram className='logo-socialMedia__Home-P4' />
+                    <BsGithub className='logo-socialMedia__Home-P4' />
                 </div>
-                <h3 className='text-instagram'>Instagram</h3>
+                <h3>Github</h3>
             </Link>
             <Link target='_blank' href='https://www.linkedin.com/in/arguelloignacio/' class="item-grid-socialMedia__Home-P4">
                 <div className='cont-logo-socialMed__Home-P4'>
                     <GrLinkedinOption className='logo-socialMedia__Home-P4' />
                 </div>
                 <h3>LinkedIn</h3>
-            </Link>
-            <Link target='_blank' href='https://github.com/ignaarguello' class="item-grid-socialMedia__Home-P4">
-                <div className='cont-logo-socialMed__Home-P4'>
-                    <BsGithub className='logo-socialMedia__Home-P4' />
-                </div>
-                <h3>Github</h3>
             </Link>
             <Link href='https://api.whatsapp.com/send?phone=1128682405' class="item-grid-socialMedia__Home-P4">
                 <div className='cont-logo-socialMed__Home-P4'>
